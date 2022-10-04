b40bdc90851c10887d873f7f2413f7772c5e1f92
chaoticgoose123 <114768079+chaoticgoose123@users.noreply.github.com>
2022-10-04 19:07:30 -0400

    Initial commit

---
+++ assets/pages/me.page.585b28fc.js
@@ -0,0 +1,2 @@
+import{j as e,a as c}from"../chunk-f4b53398.js";function a(n){const t=Object.assign({main:"main",h1:"h1",p:"p"},n.components);return c(t.main,{children:[e(t.h1,{children:"Haha"}),`
+`,e(t.p,{children:"No."})]})}function i(n={}){const{wrapper:t}=n.components||{};return t?e(t,Object.assign({},n,{children:e(a,n)})):a(n)}export{i as default};
