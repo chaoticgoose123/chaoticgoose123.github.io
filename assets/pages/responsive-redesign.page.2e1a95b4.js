63689dd0e9be03359d0bab8db9523f6163c1097b
chaoticgoose123 <114768079+chaoticgoose123@users.noreply.github.com>
2022-10-07 15:44:03 -0400

    Updated build | Oct 07, 03:43pm

---
+++ assets/pages/responsive-redesign.page.2e1a95b4.js
@@ -0,0 +1,13 @@
+import{j as e,a as t}from"../chunk-f4b53398.js";const h={title:"Responsive Redesign"};function o(i){const n=Object.assign({main:"main",h1:"h1",h2:"h2",p:"p",hr:"hr",a:"a"},i.components);return t(n.main,{children:[`
+`,`
+`,e(n.h1,{children:"Responsive Redesign"}),`
+`,e(n.h2,{children:"Introduction"}),`
+`,e(n.p,{children:"Todo"}),`
+`,e(n.h2,{children:"The Website"}),`
+`,e(n.p,{children:"Todo"}),`
+`,e(n.h2,{children:"Visual Redesign"}),`
+`,e(n.p,{children:"Todo"}),`
+`,e(n.h2,{children:"Responsive Redesign"}),`
+`,e(n.p,{children:"Todo"}),`
+`,e(n.hr,{}),`
+`,e(n.p,{children:e(n.a,{href:"#page",children:"Back to top"})})]})}function r(i={}){const{wrapper:n}=i.components||{};return n?e(n,Object.assign({},i,{children:e(o,i)})):o(i)}export{r as default,h as documentProps};
