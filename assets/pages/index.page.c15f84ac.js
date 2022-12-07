a20d3da8aa0efb5f2562315b57f2062f116f2687
chaoticgoose123 <114768079+chaoticgoose123@users.noreply.github.com>
2022-12-07 00:25:13 -0500

    Updated build | Dec 07, 12:25am

---
+++ assets/pages/index.page.c15f84ac.js
@@ -0,0 +1,10 @@
+import{j as n,a as i}from"../chunk-f4b53398.js";const s={title:"Home"};function o(r){const e=Object.assign({main:"main",h1:"h1",a:"a",p:"p",ol:"ol",li:"li",hr:"hr"},r.components);return i(e.main,{children:[`
+`,n(e.h1,{id:"hello-world",children:n(e.a,{href:"#hello-world",children:"Hello, World"})}),`
+`,i(e.p,{children:["This page functions as an index to my submissions for ",n(e.a,{href:"https://cs.brown.edu/courses/info/csci1300/",target:"_blank",rel:"noopener noreferrer",children:"CSCI 1300"}),", Fall 2022."]}),`
+`,i(e.ol,{children:[`
+`,n(e.li,{children:n(e.a,{href:"/personas-storyboarding",children:"Personas & Storyboarding"})}),`
+`,n(e.li,{children:n(e.a,{href:"responsive-redesign",children:"Responsive Redesign"})}),`
+`,n(e.li,{children:n(e.a,{href:"/iterative-design",children:"Iterative Design & Evaluation"})}),`
+`]}),`
+`,n(e.hr,{}),`
+`,i(e.p,{children:["Made with \u2764\uFE0F in Providence by ",n(e.a,{href:"/me",children:"me"}),"."]})]})}function t(r={}){const{wrapper:e}=r.components||{};return e?n(e,Object.assign({},r,{children:n(o,r)})):o(r)}export{t as default,s as documentProps};
