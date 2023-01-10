414d6f2fc6605b562aa68069cc08be4ff37889ac
chaoticgoose123 <114768079+chaoticgoose123@users.noreply.github.com>
2023-01-10 02:57:23 -0500

    Updated build | Jan 10, 02:57am

---
+++ assets/pages/index.page.c71a5d07.js
@@ -0,0 +1,84 @@
+import{j as n,a as i}from"../chunk-f4b53398.js";const a={title:"Backcasting"};function t(r){const e=Object.assign({main:"main",h1:"h1",a:"a",h2:"h2",p:"p",em:"em",ul:"ul",li:"li",strong:"strong"},r.components);return i(e.main,{children:[`
+`,n(e.h1,{id:"backcasting",children:n(e.a,{href:"#backcasting",children:"Backcasting"})}),`
+`,n(e.h2,{id:"personal-mission-statement",children:n(e.a,{href:"#personal-mission-statement",children:"Personal Mission Statement"})}),`
+`,i(e.p,{children:["Broadly speaking, I like to design ",n(e.em,{children:"things"}),". I love thinking about how things are implemented and why they work well for their intended purposes. This includes intangible things like communication itself."]}),`
+`,n(e.p,{children:"My current interest is in crafting interactive experiences. Usually, these are for education, entertainment, or the simple joy of good UX; but sometimes, I like for them to exist solely for their own sake."}),`
+`,n(e.h2,{id:"potential-future-roles--organizations",children:n(e.a,{href:"#potential-future-roles--organizations",children:"Potential Future Roles / Organizations"})}),`
+`,n(e.p,{children:"I am required to work a job in my country's public service for several years after I graduate, probably starting as a software engineer in GovTech Singapore (my assigned agency)."}),`
+`,n(e.p,{children:"I don't have much information about what I'd be doing in the other agencies I might be rotated to, so instead, here's a list of teams (and their agencies) I'm currently interested in:"}),`
+`,i(e.ul,{children:[`
+`,n(e.li,{children:"Open Government Products (GovTech Singapore)"}),`
+`,n(e.li,{children:"Government Digital Services (GovTech Singapore)"}),`
+`,n(e.li,{children:"Curriculum Planning and Development Division (Ministry of Education)"}),`
+`,n(e.li,{children:"National Climate Change Secretariat (Prime Minister's Office)"}),`
+`]}),`
+`,n(e.p,{children:"And here's a list of careers I'd be interested in outside of government work:"}),`
+`,i(e.ul,{children:[`
+`,n(e.li,{children:"UI/UX engineer"}),`
+`,n(e.li,{children:"video game developer, especially at an independent studio"}),`
+`,n(e.li,{children:"instructional designer"}),`
+`]}),`
+`,n(e.h2,{id:"potential-future-colleagues",children:n(e.a,{href:"#potential-future-colleagues",children:"Potential Future Colleagues"})}),`
+`,n(e.p,{children:"These are some of the early-career individuals I found who are doing things that aren't dissimilar to what I'd like to end up doing:"}),`
+`,i(e.ul,{children:[`
+`,i(e.li,{children:[n(e.strong,{children:"Caleb Chiam"})," (",n(e.a,{href:"https://www.linkedin.com/in/calebchiam/",target:"_blank",rel:"noopener noreferrer",children:"LinkedIn"}),") (",n(e.a,{href:"https://calebchiam.com",target:"_blank",rel:"noopener noreferrer",children:"Website"}),")",`
+`,i(e.ul,{children:[`
+`,n(e.li,{children:"SWE @ Open Government Products"}),`
+`,n(e.li,{children:"Professional profile expresses both breadth and depth of CS experience"}),`
+`,n(e.li,{children:"Clean, simple, tech-y website"}),`
+`]}),`
+`]}),`
+`,i(e.li,{children:[n(e.strong,{children:"Shipei Tan"})," (",n(e.a,{href:"https://www.linkedin.com/in/shipei-tan/",target:"_blank",rel:"noopener noreferrer",children:"LinkedIn"}),")",`
+`,i(e.ul,{children:[`
+`,n(e.li,{children:"Curriculum Planning and Development Officer @ Ministry of Education"}),`
+`,n(e.li,{children:"Several years of teaching experience in secondary schools before transitioning to curriculum planning"}),`
+`,n(e.li,{children:'"Passionate about creating transformational impact through authentic learning experiences"'}),`
+`]}),`
+`]}),`
+`,i(e.li,{children:[n(e.strong,{children:"Huiting Lee"})," (",n(e.a,{href:"https://www.linkedin.com/in/huiting-lee/",target:"_blank",rel:"noopener noreferrer",children:"LinkedIn"}),") (",n(e.a,{href:"https://huitinglee.com",target:"_blank",rel:"noopener noreferrer",children:"Website"}),")",`
+`,i(e.ul,{children:[`
+`,n(e.li,{children:"Frontend Engineer @ ByteDance"}),`
+`,n(e.li,{children:"Projects on her website are somewhat all over the place"}),`
+`,n(e.li,{children:"Website emphasizes art and design, work experience is in full-stack development (leaning frontend) with a small amount of illustration"}),`
+`]}),`
+`]}),`
+`,i(e.li,{children:[n(e.strong,{children:"Shane Staller"})," (",n(e.a,{href:"https://www.linkedin.com/in/shanestaller/",target:"_blank",rel:"noopener noreferrer",children:"LinkedIn"}),") (",n(e.a,{href:"https://stallershane.wixsite.com/portfolio",target:"_blank",rel:"noopener noreferrer",children:"Website"}),")",`
+`,i(e.ul,{children:[`
+`,n(e.li,{children:"Associate Game Designer @ Nintendo Software Technologies"}),`
+`,n(e.li,{children:"Background in game design from Digipen"}),`
+`,n(e.li,{children:"Additional experience from contract work (WWE 2k) and personal projects"}),`
+`,n(e.li,{children:"Website is rough around the edges but highlights specific level and system design work"}),`
+`]}),`
+`]}),`
+`,i(e.li,{children:[n(e.strong,{children:"Yusuf Amani"})," (",n(e.a,{href:"https://www.linkedin.com/in/yusufamani/",target:"_blank",rel:"noopener noreferrer",children:"LinkedIn"}),") (",n(e.a,{href:"https://yusufamani.github.io/portfolio/",target:"_blank",rel:"noopener noreferrer",children:"Website"}),") (",n(e.a,{href:"https://www.behance.net/yusufamani",target:"_blank",rel:"noopener noreferrer",children:"Behance"}),")",`
+`,i(e.ul,{children:[`
+`,n(e.li,{children:"UX Engineer @ LinkedIn"}),`
+`,n(e.li,{children:"Master's in Human Computer Interaction & Design from UC Irvine; it's possible that to be taken seriously in HCI you actually need something like that, since CS is much too generic and tends to lean away from UX"}),`
+`,n(e.li,{children:"Experience in user research"}),`
+`]}),`
+`]}),`
+`,i(e.li,{children:[n(e.strong,{children:"Freja Holm\xE9r"})," (",n(e.a,{href:"https://www.youtube.com/@Acegikmo/about",target:"_blank",rel:"noopener noreferrer",children:"Youtube"}),")",`
+`,i(e.ul,{children:[`
+`,n(e.li,{children:"Tech art, math, and game development Youtuber and streamer"}),`
+`,i(e.li,{children:["Long history of making videos on Youtube, which have recently become pretty smooth",`
+`,i(e.ul,{children:[`
+`,i(e.li,{children:[n(e.a,{href:"https://www.youtube.com/watch?v=aVwxzDHniEw",target:"_blank",rel:"noopener noreferrer",children:"The Beauty Of B\xE9zier Curves"})," is her most viewed video (1.4M views)"]}),`
+`]}),`
+`]}),`
+`,n(e.li,{children:"Also develops Unity packages and tools"}),`
+`]}),`
+`]}),`
+`]}),`
+`,n(e.p,{children:"Some of them have experience which I perhaps ought to consider acquiring myself:"}),`
+`,i(e.ul,{children:[`
+`,n(e.li,{children:"Backend familiarity, especially with AWS, perhaps with Flask or Node"}),`
+`,n(e.li,{children:"Teaching and policymaking experience"}),`
+`,n(e.li,{children:"Graphic and interface design projects"}),`
+`,n(e.li,{children:"Behavior trees and enemy AI, specifically, their implementation and design"}),`
+`,n(e.li,{children:"Research in user patterns, game systems design, and pedagogical practices"}),`
+`,n(e.li,{children:"Internships in related companies (N.B. these might perhaps be a little difficult to obtain)"}),`
+`,n(e.li,{children:"More Unity development skills, especially with shader graphs"}),`
+`]}),`
+`,n(e.h2,{id:"closing",children:n(e.a,{href:"#closing",children:"Closing"})}),`
+`,n(e.p,{children:"With my broad interests, I don't think that I fit neatly in any single industry or profession. My current skills, however, make it very easy for others to peg me as nothing more than a typical frontend developer."}),`
+`,n(e.p,{children:"While I'd probably be at least partially fulfilled in just about any career, I'd like to broaden my options a little, which is why I'm trying to gain more experience and generate concrete work in art and design (as opposed to in CS and engineering)."})]})}function o(r={}){const{wrapper:e}=r.components||{};return e?n(e,Object.assign({},r,{children:n(t,r)})):t(r)}export{o as default,a as documentProps};
